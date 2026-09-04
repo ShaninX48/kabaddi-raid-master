4bbebc682050f8828511435f4bad81dd2025e4a9
shanin <shaninmahmood@gmail.com>
2026-09-04 13:42:17 +0600

    Add Easy/Medium/Hard difficulty levels

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Kabaddi: Raid Master — offline support. Bump version to refresh cache. */
-const CACHE = 'raid-master-v8';
+const CACHE = 'raid-master-v9';
 const ASSETS = [
   './',
   './index.html',
